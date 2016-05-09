efc7778dc851f3ab88df8941fa23f7caac6a1701
Tom Randus <tom.randus@gmail.com>
2016-05-09 15:34:57 +0200

    Updated dashboard

--- src/www/config.js
+++ src/www/config.js
@@ -2,19 +2,19 @@ exports.template = `
 
 <div>
     <h4>Filtrace</h4>
-    Testovací retezce: <a href="http://127.0.0.1:8000/?words=mauricius">mauricius</a>,
-    <a href="http://127.0.0.1:8000/?words=maledivy">maledivy</a>,
-    <a href="http://127.0.0.1:8000/?words=seychely">seychely</a>,
-    <a href="http://127.0.0.1:8000/?words=mauricius,all inclusive">mauricius,all inclusive</a>,
-    <a href="http://127.0.0.1:8000/?words=recko">recko</a>
+    Testovaci retezce: <a href="?words=mauricius">mauricius</a>,
+    <a href="?words=maledivy">maledivy</a>,
+    <a href="?words=seychely">seychely</a>,
+    <a href="?words=mauricius,all inclusive">mauricius,all inclusive</a>,
+    <a href="?words=recko">recko</a>
     ...<br/><br/>
     <form>
-      <label>Vlastní slovo: (více slov oddel cárkou) <input name="words" id="filter"/> </label> <input type="submit" value="odeslat"\><br/>
+      <label>Vlastni slovo: (vice slov oddel carkou) <input name="words" id="filter"/> </label> <input type="submit" value="odeslat"\><br/>
     </form>
 </div>
 
 <div>
-    <h4>Vlož kód do stránky:</h4>
+    <h4>Vlozte kod do stranky:</h4>
     <code>
         &lt;script><br/>
             &nbsp;var filter="<%=words%>";<br/>
@@ -28,15 +28,15 @@ exports.template = `
 </div>
 
 <div>
-    <h4>Další možná konfigurace</h4>
+    <h4>Dalsi mozne konfigurace</h4>
     <code>
-    &nbsp;var maxCount=10; //Pocet zobrazených položek (default 20)<br/>
+    &nbsp;var maxCount=10; //Pocet zobrazenych polozek (default 20)<br/>
     &nbsp;var columns=2; //Pocet sloupcu (default 2)<br/>
     </code>
 </div>
 
 <div>
-    <h4>Náhled:</h4>
+    <h4>Nahled:</h4>
     <script type="text/javascript">
         var filter = "<%=words%>";
     </script>
